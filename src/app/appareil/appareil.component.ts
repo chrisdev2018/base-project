2799e81f4552d4c73fbaff342b691c08bd2e292b
Christian FOMEKONG <christian.fomekong@gmail.com>
2019-06-06 23:29:31 +0100

    [IMP]Premiers tests

---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.css']
+})
+export class AppareilComponent implements OnInit {
+
+  constructor() { }
+
+  nom: string = "christian"
+  ngOnInit() {
+  }
+
+
+
+}
