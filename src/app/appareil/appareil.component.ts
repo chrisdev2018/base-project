dad848c2eaca8843d8cf96fbe899409804d504ef
Christian FOMEKONG <christian.fomekong@gmail.com>
2019-06-08 11:18:15 +0100

    [IMP]premiere partie tuto OK

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-appareil',
@@ -7,9 +8,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppareilComponent implements OnInit {
 
-  constructor() { }
+  isAuth = false;
+  recup: string = "";
 
-  nom: string = "christian"
+  constructor(){
+    setTimeout(
+      ()=>{
+        this.isAuth = true;
+      }, 5000
+    );   
+  }
+ 
+  nom: string = ""
+  prenom: string = ""
+
+  allumer()
+  {
+    if(this.nom != "" && this.prenom !=""){
+      this.etudiants.push(
+        {
+          mat: this.nom,
+          nom: this.prenom,
+          note: 0
+        }
+      );
+    }
+
+    this.nom = "";
+    this.prenom = "";
+  }
+
+  etudiants = [
+    {
+      mat: "001",
+      nom: "fomekong",
+      note: 12
+    },
+    {
+      mat: "005",
+      nom: "mongo",
+      note: 15
+    },
+    {
+      mat: "006",
+      nom: "alex",
+      note: 15
+    },
+    {
+      mat: "009",
+      nom: "charly",
+      note: 5
+    },
+    {
+      mat: "005",
+      nom: "martial",
+      note: 10
+    },
+    {
+      mat: "005",
+      nom: "carole",
+      note: 2.5
+    },
+    {
+      mat: "005",
+      nom: "bertin",
+      note: 6.5
+    },
+    {
+      mat: "005",
+      nom: "henri",
+      note: 14.5
+    }
+  ]
+  
+  lastDate = new Date();
+  
   ngOnInit() {
   }
 
